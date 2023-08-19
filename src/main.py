1d997abee389a0e0aebf29228e87eff04ff282e3
idzingirai <idzingirai@dvt.co.za>
2023-08-19 18:23:56 +0200

    Updated main class

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import sys
 
 from config import SEED, PROBLEM_INSTANCES_INDICES
 from problem import Problem
+from src.constraints_validator import Validator
+from timetable import Timetable
 
 if __name__ == "__main__":
     #   Seed the whole program
@@ -12,4 +14,10 @@ if __name__ == "__main__":
     for problem_instance_index in PROBLEM_INSTANCES_INDICES:
         problem_instance = Problem(problem_instance_index=problem_instance_index)
         problem_instance.initialize()
+
+        timetable = Timetable(problem=problem_instance)
+
+        validator = Validator(timetable)
+        print(validator.get_violated_hard_constraints())
+
         break
